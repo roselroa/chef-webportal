9dddeb35b76e67f6d5b5c27e2fd23b673df02103
roselroa <rosel@cascadeo.com>
2017-02-11 10:41:40 +0800

    added app

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,14 +5,21 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
+# Install web service package
 case node['platform']
 when 'redhat', 'centos', 'amazon', 'scientific', 'oracle'
 	package 'httpd'
+when 'ubuntu', 'debian'
+	package 'apache2'
+end
+
+# Start web service
+case node['platform']
+when 'redhat', 'centos', 'amazon', 'scientific', 'oracle'
 	service 'httpd' do
 		action [:enable, :start]
 	end
 when 'ubuntu', 'debian'
-	package 'apache2'
 	service 'apache2' do
 		action [:enable, :start]
 	end
