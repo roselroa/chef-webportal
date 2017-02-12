fa6a4fb2f4e443c51414237e64989ed02fe33f7d
roselroa <rosel@cascadeo.com>
2017-02-12 16:10:19 +0800

    clean up

--- cookbooks/db/metadata.rb
+++ cookbooks/db/metadata.rb
@@ -5,16 +5,5 @@ license 'all_rights'
 description 'Installs/Configures db'
 long_description 'Installs/Configures db'
 version '0.1.0'
+
 depends 'mysql', '~> 8.2'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/db/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/db' if respond_to?(:source_url)
