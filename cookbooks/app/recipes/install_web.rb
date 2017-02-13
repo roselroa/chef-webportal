398f58c637a5fb05e98276393a935475adb10bfc
Rosel Roa <rosel@cascadeo.com>
2017-02-14 04:27:27 +0800

    added web color data item for dev,stg,prd

--- cookbooks/app/recipes/install_web.rb
+++ cookbooks/app/recipes/install_web.rb
@@ -3,6 +3,7 @@
 mysql = data_bag_item('passwords','mysql')
 webapp = data_bag_item('passwords','webapp')
 db_ip = data_bag_item('servers','db')
+web_color = data_bag_item('web','color')
 
 file '/var/tmp/secret' do
 	content "#{mysql[node.chef_environment]} #{webapp[node.chef_environment]} #{db_ip[node.chef_environment]}"
@@ -27,7 +28,7 @@ cookbook_file '/var/www/html/info.php' do
 end
 
 template '/var/www/html/index.php' do
-	variables( :webapp_access => webapp[node.chef_environment], :webapp_db => db_ip[node.chef_environment] )
+	variables( :webapp_access => webapp[node.chef_environment], :webapp_db => db_ip[node.chef_environment], :web_color => web_color[node.chef_environment] )
 	source 'index.erb'
 	owner 'root'
 	group 'root'
