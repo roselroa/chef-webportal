f8428af87629473ee1013984965653e9ebd80826
root <root@web1.(none)>
2017-02-14 02:48:35 +0800

    added schema without data

--- cookbooks/db/files/db_schema.sql
+++ cookbooks/db/files/db_schema.sql
@@ -26,18 +26,8 @@ CREATE TABLE `page_visitor_ip` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip_addr` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `page_visitor_ip`
---
-
-LOCK TABLES `page_visitor_ip` WRITE;
-/*!40000 ALTER TABLE `page_visitor_ip` DISABLE KEYS */;
-INSERT INTO `page_visitor_ip` VALUES (1,'192.168.0.100'),(2,'192.168.0.101'),(3,'192.168.0.101'),(4,'192.168.0.101'),(5,'192.168.0.100'),(6,'192.168.0.100'),(7,'192.168.87.1'),(8,'192.168.87.1'),(9,'192.168.87.1'),(10,'192.168.87.1'),(11,'192.168.87.1'),(12,'192.168.87.1'),(13,'192.168.87.1'),(14,'192.168.87.1'),(15,'192.168.87.1'),(16,'192.168.87.1'),(17,'192.168.87.1'),(18,'192.168.87.1'),(19,'192.168.87.1');
-/*!40000 ALTER TABLE `page_visitor_ip` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +38,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-13 19:22:09
+-- Dump completed on 2017-02-14  2:46:20
